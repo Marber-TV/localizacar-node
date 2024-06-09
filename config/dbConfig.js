a20093498afbd7f8a3473ef2d249df9108a70a56
Pablo Martinez <pblmartinezber@gmail.com>
2024-06-10 00:20:17 +0200

    Configuración corregida para usar Fixie proxy

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,24 +1,9 @@
 const { Pool } = require('pg');
-const HttpsProxyAgent = require('https-proxy-agent');
-const url = require('url');
-
-// Obtener la URL del proxy de Fixie desde la variable de entorno
-const proxyUrl = process.env.FIXIE_URL;
-
-if (!proxyUrl) {
-    throw new Error('FIXIE_URL no está definida');
-}
-
-const agent = new HttpsProxyAgent(proxyUrl);
 
 const dbConfig = {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: 'postgres://u806htc6eqbi4v:pbbd75b1deec5a78024760fa69171d3cf59ca21903040bf5b9af9bce48c30e44e@cav8p52l9arddb.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d7vd377320nl6h',
     ssl: {
         rejectUnauthorized: false
-    },
-    // Añadir el proxy a la configuración de la base de datos
-    connection: {
-        agent: agent
     }
 };
 
