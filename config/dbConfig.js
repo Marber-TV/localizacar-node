1dab9eb153c35e06489a88fc6d894a72b9213bc8
Pablo Martinez <117436571+Marber-TV@users.noreply.github.com>
2024-06-14 09:00:57 +0200

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,7 +1,6 @@
 const { Pool } = require('pg');
 
 const dbConfig = {
-    connectionString: 'postgres://u806htc6eqbi4v:pbbd75b1deec5a78024760fa69171d3cf59ca21903040bf5b9af9bce48c30e44e@cav8p52l9arddb.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d7vd377320nl6h',
     ssl: {
         rejectUnauthorized: false
     }
